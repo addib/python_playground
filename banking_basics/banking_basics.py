b5da6643f5b176b891381e0505be338e4b2d725e
Dibakar Aditya <dibakar.d.aditya@microfocus.com>
2019-04-02 22:31:22 +0530

    Add attributes and methods to the class

--- banking_basics/banking_basics.py
+++ banking_basics/banking_basics.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from random import randint
 
 # abstract base class to enforce methods on the derived child class SavingsAccount
 class Account(metaclass = ABCMeta):
@@ -22,14 +23,38 @@ class Account(metaclass = ABCMeta):
 class SavingsAccount:
     def _init_(self):
         self.savings_accounts = {}
-    def create_account(self):
-        pass
-    def authenticate_user(self):
-        pass
-    def withdraw_sum(self):
-        pass
-    def deposit_sum(self):
-        pass
-    def display_balance(self):
-        pass
+
+    def create_account(self, name, initial_deposit):
+        self.account_number = randint(1000000000, 9999999999)
+        self.savings_accounts[self.account_number] = [name, initial_deposit]
+
+    def authenticate_user(self, name, account_number):
+        if account_number in self.savings_accounts.keys():
+            if self.savings_accounts[self.account_number][0] == name:
+                print("Authentication successful")
+                self.account_number = account_number
+                return True
+            else:
+                print("Name does not match")
+                return False
+        else:
+            print("Account number not valid")
+            return False
+
+    def withdraw_sum(self, withdrawl_sum):
+        if withdrawl_sum > self.savings_accounts[self.account_number][1]:
+            print("Insufficient balance")
+        else:
+            self.savings_accounts[self.account_number][1] -= withdrawl_sum
+            print("Withdrawl successful. Available balance: ", end = " ")
+            self.display_balance()
     
+    def deposit_sum(self, deposited_sum):
+        self.savings_accounts[self.account_number][1] += deposited_sum
+        print("Deposit successful. Available balance: ", end = " ")
+        self.display_balance()
+
+    def display_balance(self):
+        print("Available balance: ", self.savings_accounts[self.account_number][1])
+
+SavingsAccount = SavingsAccount()
